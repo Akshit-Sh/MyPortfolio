4be882fa9b3fc92cc289caf9dc338418a323531f
Akshit <akshitsharma20022@gmail.com>
2022-08-03 14:50:36 +0530

    updated resume

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -4,7 +4,7 @@ function Resume() {
     return (
         <div id="resumeComp" className=" resumePlacement resumeDiv container d-flex justify-content-center align-items-center" >
             <button className="resumeBtn">
-                <a href="https://drive.google.com/file/d/18_MlNs99O8s32JblityLzcQZyiGpRDwx/view" target="__blank" ><i className="fa fa-arrow-circle-down"></i> &nbsp; Download Resume</a>
+                <a href="https://drive.google.com/file/d/1HCyi0mgNiBzfdVxuYAzvXhMbfZZtn6Am/view?usp=sharing" target="__blank" ><i className="fa fa-arrow-circle-down"></i> &nbsp; Download Resume</a>
             </button>
         </div>
     )
